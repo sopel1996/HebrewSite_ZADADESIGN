58a3cd573459b946cb2b26bd5c80fae84c83dd97
sopel1996 <sopel@live.ru>
2021-07-26 23:34:25 +0300

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+// const postcss = require('gulp-postcss');
 
 function style() {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+			cascade: false
+		}))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 }
