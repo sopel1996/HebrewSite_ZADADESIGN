19fa65e66460b31365b2ca40b7876f5a46cb9919
sopel1996 <sopel@live.ru>
2021-07-29 22:36:14 +0300

    Add masonry, stiky menu. Fix margin and padding

--- js/animation.js
+++ js/animation.js
@@ -2,9 +2,10 @@ function onEntry(entry) {
     entry.forEach(change => {
         if (change.isIntersecting) {
             change.target.classList.add('element-show');
-        } else {
-            change.target.classList.remove('element-show');
-        }
+        } 
+        // else {
+        //     change.target.classList.remove('element-show');
+        // }
     });
 }
 
